4ced5a8cbf92d01077f27c3f301f64ac6d918304
Praesentia-YKM <ykm8864@naver.com>
2024-12-16 13:13:07 +0900

    Initial commit

---
+++ src/main/java/com/domainPractice/drinkOrderDomain/DrinkOrderDomainApplication.java
@@ -0,0 +1,13 @@
+package com.domainPractice.drinkOrderDomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkOrderDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkOrderDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domainPractice/drinkOrderDomain/DrinkOrderDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domainPractice.drinkOrderDomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkOrderDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
